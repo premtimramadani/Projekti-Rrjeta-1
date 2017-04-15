b1afa8d04290d1cab81d19a162d188b3098951ba
premtimramadani <premtimr@outlook.com>
2017-04-15 19:41:05 +0200

    Ndryshime
    Do ndryshime trea

--- TcpKlienti/TcpKlienti.py
+++ TcpKlienti/TcpKlienti.py
@@ -12,43 +12,60 @@ serverName = "127.0.0.1"
 serverPort = 9000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-print ("Zgjedh nje opcion :  \n IP \n PORT \n HOST \n KOHA  \n KENO  \n ")
-opcioni = input("")
 
+print("Zgjedh nje opcion :  \n IP \n PORT \n HOST \n KOHA  \n KENO  \n KONVERTO \n ")
+print("-----------------------\n")
+opcioni=input()
+#me ba case insensitive, e kthejm cdo input nshkronja tmdhaja
+opcioniUP=str(opcioni).upper()
+#funksioni i cili dergon te dhenat ne server dhe merr nga ai pergjigjien
+def konvertoServer():
+    clientSocket.send(konvertimi.encode('ASCII'))
+    pergjigjiaServerit = clientSocket.recv(1024)
+    opcioni,numri=konvertimi.split(' ')
+    print("KONVERTO",opcioni,numri,"kthen pergjigjie",
+    pergjigjiaServerit.decode('ASCII'))
                 
-if opcioni=="IP":
-    clientSocket.send(opcioni.encode('ASCII'))
+if opcioniUP=="IP":
+    clientSocket.send(opcioniUP.encode('ASCII'))
     modifiedOpcioni = clientSocket.recv(1024)
     print("From Server:", modifiedOpcioni.decode('ASCII'))
     clientSocket.close()
-elif opcioni=="PORT":
-    clientSocket.send(opcioni.encode('ASCII'))
-    modifiedOpcioni = clientSocket.recv(1024)
-    print("From Server:", modifiedOpcioni.decode('ASCII'))
-    clientSocket.close()
-
-elif opcioni=="HOST":
-    clientSocket.send(opcioni.encode('ASCII'))
+elif opcioniUP=="PORT":
+    clientSocket.send(opcioniUP.encode('ASCII'))
     modifiedOpcioni = clientSocket.recv(1024)
     print("From Server:", modifiedOpcioni.decode('ASCII'))
     clientSocket.close()
 
-elif opcioni=="KOHA":
-    clientSocket.send(opcioni.encode('ASCII'))
+elif opcioniUP=="HOST":
+    clientSocket.send(opcioniUP.encode('ASCII'))
     modifiedOpcioni = clientSocket.recv(1024)
     print("From Server:", modifiedOpcioni.decode('ASCII'))
     clientSocket.close()
 
-elif opcioni=="KENO":
-    clientSocket.send(opcioni.encode('ASCII'))
+elif opcioniUP=="KOHA":
+    clientSocket.send(opcioniUP.encode('ASCII'))
     modifiedOpcioni = clientSocket.recv(1024)
     print("From Server:", modifiedOpcioni.decode('ASCII'))
     clientSocket.close()
 
+elif opcioniUP=="KENO":
+    clientSocket.send(opcioniUP.encode('ASCII'))
+    modifiedOpcioni = clientSocket.recv(1024)
+    print("From Server:", modifiedOpcioni.decode('ASCII'))
+elif opcioniUP=="KONVERTO":
+    konvertimi = input("KONVERTO ")
+    if konvertimi=="?":
+        print("-----------------------\n")
+        print("CelsiusToKelvin\n","CelsiusToFahrenheit\n","KelvinToFahrenheit\n",
+            "KelvinToCelsius\n","FahrenheitToCelsius\n","FahrenheitToKelvin\n",
+            "PoundToKilogram\n","KilogramToPound\n")
+        print("-----------------------\n")
+        konvertimi = input("KONVERTO ")
+        #thirrja e funksionit per dergim dhe pranim te te dhenave me serverin 
+        konvertoServer()
+    else:
+        konvertoServer()
 
 
-
-  
-
-
-
+clientSocket.close()
